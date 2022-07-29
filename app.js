be2c3d063ba4dd31561eaab2169953083b459691
roshramos <rosh.ramos30@hotmail.com>
2022-07-29 23:20:32 +1000

    Halfway there - Need to watch Promises Videos

---
+++ app.js
@@ -0,0 +1,84 @@
+//Q1 SHOW RATING
+//Given a rating, display a star (*) for each full rating and a full-stop (.) for each half rating.
+
+function showRating (rating) {
+    let ratings = "";
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratings += "*";
+        if (i !== Math.floor(rating) - 1) {
+            ratings += " "
+        }
+    }
+    if (!Number.isInteger (rating)) {
+        ratings += " ."
+    }
+    return ratings;
+}
+
+console.log (showRating(2.5))
+
+
+/** STEPS:
+ * Initialise and empty string
+ * loop throught he rounded down rating
+ * add a star for every iteration
+ * if its not the last iteration add a space
+ * if the number is not an integer
+ * add a full stop
+ * return it
+ */
+
+
+//Q2 SORT BY LOWEST TO HIGHEST PRICE 
+//Given an array of numbers, return the prices sorted by low to high.
+
+function sotLowToHigh (numbers) {
+    return numbers.sort((a, b) => a - b)
+}
+
+console.log (sotLowToHigh([20, 40, 10, 30, 50, 10]))
+
+
+//Q3 SORT BY HIGHEST TO LOWEST PRICE
+//Given an array of objects, return the prices sorted by high to low.
+
+function sortHighToLow (numbers) {
+ return numbers.sort((a, b) => b.price - a.price)
+}
+
+console.log (sortHighToLow([
+    { id: 1, price: 50},
+    { id: 2, price: 0},
+    { id: 3, price: 500},
+ ])
+ )
+
+
+//Q4 PROMISES
+//
+
+function asdf () {
+
+}
+
+console.log (asdf())
+
+
+//Q5 FIND ALL THE POSTS BY A SINGLE USER
+//Call this API "https://jsonplaceholder.typicode.com/posts" and return all the posts by any given user Id.
+
+function postsByUser () {
+
+}
+
+console.log (postsByUser())
+
+
+//Q6 FIRST 6 INCOMPLETE TODOS
+//Call this API "https://jsonplaceholder.typicode.com/todos" and return the first 6 incomplete todo's from the result.
+
+function firstSixIncomplete () {
+
+}
+
+console.log (firstSixIncomplete())
